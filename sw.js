9d0fbf4e9f07c9504c3e2e2a0aad7061613e5f10
jczanella <juliozanella@gmail.com>
2026-05-19 12:29:57 -0300

    Novo simulador CDC Campanha — Linha CE (Leves/Pesados, Price, 0% até 12m)

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ const ARQUIVOS = [
   '/simuladores/',
   '/simuladores/index.html',
   '/simuladores/CE_maisinovacao.html',
+  '/simuladores/CE_cdccampanha.html',
   '/simuladores/manifest.json',
   '/simuladores/icon-192.png',
   '/simuladores/icon-512.png',
