653ffa2aa64dbcb3dab0b3834a6e153edaad463b
Julio Cesar Zanella <julio.zanella@pme.local>
2026-07-31 13:43:38 -0300

    Adiciona simulador Move Agrícola (Finep Aquisição Inovadora)

--- sw.js
+++ sw.js
@@ -7,6 +7,7 @@ const ARQUIVOS = [
   '/simuladores/CE_pronampce.html',
   '/simuladores/CE_cdccampanha.html',
   '/simuladores/CE_cdcbalcao.html',
+  '/simuladores/AG_moveagricola.html',
   '/simuladores/manifest.json',
   '/simuladores/icon-192.png',
   '/simuladores/icon-512.png',
